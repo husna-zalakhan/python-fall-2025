0408b2fc26c7f72e012fbf97365dafdc35b7b6f6
husna-zalakhan <husna.zalakhan@gmail.com>
2026-01-11 00:06:08 +0000

    Class Work 6 py

---
+++ ClassWork6.py
@@ -0,0 +1,29 @@
+# If statement
+# temprature = 25
+
+# if temprature < 30:
+#     print("hi")
+
+# score=40
+# if score==90: #comparison opertor
+#     print ("A")
+
+# if score>=80:
+#     print("B")
+
+# if score<50:
+#     print ("C")
+ 
+#Elif 
+score=90
+
+if score>=90:
+    print("A")
+
+elif score>=80 and score<90:
+    print("B")
+
+elif score<80:
+    print("C")
+
+
