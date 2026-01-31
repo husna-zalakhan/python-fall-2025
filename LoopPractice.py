3246e11fd80fca61e21099c8f9d1ff93fab060ec
husna-zalakhan <husna.zalakhan@gmail.com>
2026-01-31 09:20:03 +0000

    py functions

--- LoopPractice.py
+++ LoopPractice.py
@@ -5,3 +5,28 @@
 #while loop
 #do while loop: py doesn't have do while loop
 
+#functions
+# function: has input and output
+#syntax: def + function name (parameter1, parameter2):
+def say_hello():
+    print("Hello!") 
+
+say_hello()
+say_hello()
+say_hello()
+
+#Arguments in functions
+def return_explination(number):
+    result=number*2
+    return result
+
+print(return_explination(1)) #first method
+x=return_explination(1) 
+print(x) #second method
+
+y=return_explination(3) 
+print(y)
+
+z= return_explination(1)+1-2
+print(z) 
+
